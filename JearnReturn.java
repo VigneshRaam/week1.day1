0e06b61acd4219ac52e1277893fdf188d2b1b674
VigneshRaam <91826194+VigneshRaam@users.noreply.github.com>
2021-10-16 12:31:57 +0530

    Add files via upload
    Added Learn Return

---
+++ JearnReturn.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class JearnReturn {
+
+	public static void main(String[] args) {
+		boolean value = true;
+		if (value ==true) {
+			System.out.println("value is true");
+		}
+	}
+
+}
